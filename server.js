c2f25d19bfdc44494eea80fe897ba9f6a342c06f
Marcelo Manzano <manzanomarcelo@outlook.com>
2026-04-24 18:36:42 +0000

    add feature and flags API

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
+
+const flagsPath = path.join(__dirname, "flags.json");
+
+app.get("/api/features", (req, res) => {
+  const rawFlags = fs.readFileSync(flagsPath, "utf-8");
+  const flags = JSON.parse(rawFlags);
+
+  res.json(flags);
+});
 
 app.use(express.static("public"));
 
